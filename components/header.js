e27a0764836d0323018df53f60355fb750dd05a6
Graungaard <kristensen8@hotmail.com>
2022-02-19 14:14:47 +0100

    Update header.js

--- components/header.js
+++ components/header.js
@@ -4,11 +4,9 @@ export default function Header() {
   return (
     <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
       <Link href="/">
-<<<<<<< HEAD
+
         <a className="hover:underline">Graungaard</a>
-=======
-        <a className="hover:underline">Graungaards</a>
->>>>>>> 36f545d50faad214464f095ba4fca4d1241fa61f
+
       </Link>
       .
     </h2>
